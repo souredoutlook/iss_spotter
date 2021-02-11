888d9695781f07b5a6d54ec35a51e8d262a6bbff
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-11 21:52:39 +0000

    refactored iss.js to export fetchISSFlyOverTimes and fetCoordsByIP api calls, refactored index.js for testing purposes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 
-// const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes} = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,3 +9,20 @@
 //   }
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+// fetchCoordsByIP('108.168.19.76', (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates: ', data);
+// });
+
+// fetchISSFlyOverTimes({ longitude: -80.4995, latitude: 43.4325 }, (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times!: ', data);
+// });
+
--- iss.js
+++ iss.js
@@ -21,5 +21,48 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) =>{
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching GEOIP Response : ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      const {longitude, latitude} = JSON.parse(body);
+      callback(null, {longitude, latitude});
+    }
+  });
+};
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`,(error, response, body)=>{
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching GEOIP Response : ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data.response);
+    }
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
